28a460bb0c2635ea507d7f9843f787e61c80765d
Liam <liamsuu14974@gmail.com>
2024-09-18 22:34:52 +0100

    Change port

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use("/", homeRouter.router);
 app.use("/new", newMessageRouter);
 
 // setting up server port listener
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, "0.0.0.0", () =>
   console.log(`Server listening on port ${port}!`)
 );
